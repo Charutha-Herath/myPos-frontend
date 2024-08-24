9de254a90cc8e52b5c4d4ff1c2e4000f8b9ae9d7
Charutha-Herath <charuh2023@gmail.com>
2024-08-25 01:26:47 +0530

    updated user api

---
+++ mvc/api/userApi.js
@@ -0,0 +1,61 @@
+
+
+export class UserApi{
+
+    getUser(userName,userPassword){
+        if (userName) {
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/user',
+                data: {
+                    action: 'checkUser',
+                    userName: userName
+
+                },
+                success: function (responseText) {
+                    let user = responseText;
+                    console.log("responseText :",user , " userName ",user.username)
+                    if (typeof responseText === 'string') {
+                        // If it's a string, parse it as JSON
+                        user = JSON.parse(responseText);
+                        console.log("check responseText")
+                    }
+                    console.log("parameters ",userName,"  ",userPassword)
+                    console.log("before if ",user.username,"  ",user.password)
+                    if (userName === user.username && userPassword === user.password) {
+                        const loadingScreen = $('#login_page');
+                        loadingScreen.hide();
+
+                        const loadingScreen5 = $('#sign_up_page');
+                        loadingScreen5.hide();
+                        console.log("loading screen nav and home")
+                        const loadingScreen1 = $('#header-nav');
+                        loadingScreen1.show();
+
+                        const loadingScreen2 = $('#sec-main-home');
+                        loadingScreen2.show();
+
+                        const loadingScreen3 = $('#sec-main-store');
+                        loadingScreen3.hide();
+
+                        const loadingScreen4 = $('#sec-main-customer');
+                        loadingScreen4.hide();
+
+                        const loadingScreen6 = $('#sec-main-orderDetails');
+                        loadingScreen6.hide();
+
+                        const loadingScreen7 = $('#sec-main-place-order');
+                        loadingScreen7.hide();
+                    }else {
+                        alert("Incorrect Username Or Password");
+
+                    }
+
+                },
+                error: function (xhr, status, error) {
+                    console.log(error,"userApi..");
+                }
+            });
+        }
+    }
+}
